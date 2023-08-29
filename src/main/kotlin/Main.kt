0f9d10f0812b340a42f3ed371b0ffd5751d24845
dgriscow <dgrischow@students.mchenry.edu>
2023-08-29 16:28:04 -0500

    finnished arrays

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,8 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    var inventory = arrayListOf<String>("Buns", "Ketchup", "Mustard", "Dogs", "Relish", "Onions")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    for (item in inventory){
+        println(item)
+
+    }
 }
